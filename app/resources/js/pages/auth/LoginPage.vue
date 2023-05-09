2e580e912a73f0e9b9f5da7a8dfefbf0caddd087
MaximeGNitic <129794633+MaximeGNitic@users.noreply.github.com>
2023-05-09 11:49:26 -0400

    base de projet

---
+++ app/resources/js/pages/auth/LoginPage.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h4 class="text-blue-grey-2 text-center">Login</h4>
+        <form @submit="loginForm">
+            <input class="bg-blue-grey-2" label="Email" v-model="email"/>
+            <input class="bg-blue-grey-2" label="Mot de passe" v-model="password"/>
+
+            <btn class="bg-blue-grey-2" label="Submit" type="submit"/>
+        </form>
+    </div>
+</template>
+
+<style>
+
+</style>
---
+++ app/resources/js/pages/auth/RegisterPage.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h4>Register</h4>
+        <form @submit="registerForm">
+            <input class="bg-blue-grey-2" label="Nom" v-model="nom"/>
+            <input class="bg-blue-grey-2" label="Email" v-model="email"/>
+            <input class="bg-blue-grey-2" label="Mot de passe" v-model="password"/>
+            <input class="bg-blue-grey-2" label="Confirmer le mot de passe" v-model="confirmPassword"/>
+
+            <btn class="bg-blue-grey-2" label="Submit" type="submit"/>
+        </form> 
+    </div>
+</template>
+
+<style>
+
+</style>
